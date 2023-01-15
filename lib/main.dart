b310e3d5a01568667b8a3c8ea887ccf0af1744d6
charlie123446 <7eleven123446@gmail.com>
2023-01-16 02:12:05 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '期末專題'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var canDo = ["釣魚", "出去晃晃", "滑一下IG", "去下載一個遊戲玩看看", "找一個人聊天"];
+  var result = "";
+
+  void doSth() {
+    setState(() {
+      var rng = Random();
+      result = canDo[rng.nextInt(canDo.length)];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '你現在可以做誰麼:',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '$result',
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                  backgroundColor: Colors.yellow),
+            ),
+            TextButton(
+              onPressed: doSth,
+              child: Text(
+                "告訴我吧",
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
